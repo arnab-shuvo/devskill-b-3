fbc5ce290493fc3ae8516c9a0a5accf1e7be30e6
shadman17 <shadman.wasif@gmail.com>
2022-02-24 20:02:14 +0600

    Updated

--- shadman/api-integration/src/components/Home.jsx
+++ shadman/api-integration/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
@@ -15,57 +15,26 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ProductDetails from "./ProductDetail";
+import ProductList from "./ProductList";
 
 const Home = ({ productList, setProductList }) => {
+  const [flag, setFlag] = useState(true);
   const [productDetail, setProductDetail] = useState(null);
 
   const getDetail = (id) => {
-    fetch(`https://fakestoreapi.com/products/${id}`)
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json);
-        setProductDetail(json);
-      });
-  };
-
+    setFlag(false)
+    setProductDetail(id)
+  }
+    
   return (
-    <Grid container justifyContent={"center"}>
-      <Grid item container md={10} spacing={2}>
-        {productList.map((product) => {
-          return (
-            <Grid md={3} item onClick={() => getDetail(product.id)}>
-              <Card sx={{ maxWidth: 345 }}>
-                <CardHeader
-                  avatar={
-                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                      {product.title[0]}
-                    </Avatar>
-                  }
-                  action={
-                    <IconButton aria-label="settings">
-                      <MoreVertIcon />
-                    </IconButton>
-                  }
-                  title={product.title}
-                  subheader={product.category}
-                />
-                <CardMedia
-                  component="img"
-                  height="194"
-                  image={product.image}
-                  alt="Paella dish"
-                />
-                <CardContent>
-                  <Typography variant="body2" color="text.secondary">
-                    {product.description}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          );
-        })}
-      </Grid>
-    </Grid>
+    <>
+      {flag ? (
+        <ProductList getDetail={getDetail} />
+      ) : (
+        <ProductDetails productDetail={productDetail} setFlag={setFlag}/>
+      )}
+    </>
   );
 };
 
