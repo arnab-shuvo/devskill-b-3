6f88b4d28aa73dd180d824bee1ed8c172686160a
Shovon <parvezrahmanshovon@gmail.com>
2022-03-03 19:28:53 +0600

    first commit

--- shovon/to_do/src/Component_style.jsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const List_Item = styled.li`
-  padding: 1em 2em;
-  background-color: #fff9f3;
-  box-shadow: 0.1em 0.1em 0.1em rgb(242, 242, 242);
-`;
