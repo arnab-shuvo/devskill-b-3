5c6a560af7b0c91eb5b84928f55d04649195a1ad
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2022-03-14 17:41:38 +0600

    redux1 assignment is completed

---
+++ Arifur_Rahaman/router-app/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import { Container } from '@mui/material';
+import React from 'react';
+import SelectDropdown from '../../Components/SelectDropdown/SelectDropdown';
+
+const Home = () => {
+    return (
+        <Container>
+            <SelectDropdown></SelectDropdown>
+            <h1>Home pages</h1>
+        </Container>
+    );
+};
+
+export default Home;
