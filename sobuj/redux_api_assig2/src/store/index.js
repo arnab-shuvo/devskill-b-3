fe004b4a58654ed91309fb5d425b2774fb19d30a
Mohammad Raihanul Hoque Sabuj <raihan.sabuj@yahoo.com>
2022-04-02 19:44:06 +0600

    dev skill update by Raihan Sabuj

--- sobuj/redux_api_assig2/src/store/index.js
+++ sobuj/redux_api_assig2/src/store/index.js
@@ -1,4 +1,11 @@
-import { createStore } from "redux";
+import { width } from "@mui/system";
+import { createStore, compose, applyMiddleware } from "redux";
 import reducer from "./reducer";
+import thunk from "redux-thunk";
 
-export const store = createStore(reducer);
+const composeEnhencer =  window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
+
+export const store = createStore(
+    reducer, 
+    composeEnhencer(applyMiddleware(thunk))
+);
--- sobuj/redux_counter/src/store/index.js
+++ sobuj/redux_counter/src/store/index.js
@@ -1,5 +1,19 @@
-import {createStore} from "redux"
+import {createStore, compose, applyMiddleware} from "redux"
 import ActionType from "./action/actionType";
 import reducers from "./reducer";
+import thunk from "redux-thunk";
 
-export const store = createStore(reducers);
+// const composeEnhancer =  window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
+
+// export const store = createStore( 
+//     reducers, 
+//     composeEnhancer(applyMiddleware(thunk))
+// );
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() || compose;
+
+export const store = createStore(
+    //reducers, composeEnhancer(applyMiddleware(thunk)) //not working
+    reducers, composeEnhancer //working
+    
+  );
